b3b4f8e095f74a86790b41150de73081c0f7e9c1
layssamello22-design <layssamello6@gmail.com>
2026-05-06 21:57:46 -0400

    java

---
+++ Exer04.java
@@ -0,0 +1,8 @@
+public class Exer04 {
+    public static void main(String[] args) {
+        //tabuada do 9
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("9" + "x" +i + " = " + (9*i));
+        }
+    }
+}
