d4214654f433d6e4b455455e1b231acc7320e724
layssamello22-design <layssamello6@gmail.com>
2026-05-12 22:16:28 -0400

    exercícios

---
+++ src/Exer3_1.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+
+public class Exer3_1 {
+    public static void main(String[] args){
+        //valor para o qual o fatorial será calculada
+        int valor =  10;
+
+        long resposta = 1;
+
+        for (int i = 2; i < valor; i++) {
+            resposta *= i;
+        }
+        System.out.printf(" O fatorial de %d igual a %d " , valor, resposta);
+
+    }
+
+
+
+
+
+
+}
